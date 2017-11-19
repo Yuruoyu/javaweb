96172a26d48a588e2e8cc84b050e4d59f161ebc6
Yuruoyu <1046695612@qq.com>
2017-11-19 16:48:29 +0800

    add cat and dog add dependencys mysql and jpa to in pom.xml

---
+++ springboothello-hot/src/main/java/com/ryan/springboothello/demo/bean/Cat.java
@@ -0,0 +1,42 @@
+package com.ryan.springboothello.demo.bean;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+/**
+ * 创建一个实体类
+ * 
+ * 使用Entity　进行持久化
+ * 当JPA检测到我们的实体类当中有@Entity时候，会在数据库中生成对应的表结构信息
+ * 
+ * 
+ * @author Laird
+ *
+ */
+@Entity
+public class Cat {
+	
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	private String Name;
+	private int catAge;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public int getCatAge() {
+		return catAge;
+	}
+	public void setCatAge(int catAge) {
+		this.catAge = catAge;
+	}
+}
